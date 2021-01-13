51375a992d5777d4eaef83507e2384fe5ba88954
The-Nusbaum <38957691+The-Nusbaum@users.noreply.github.com>
2021-01-13 15:48:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 function get_files {
-  files=$(git --no-pager diff --name-only FETCH_HEAD)
+  files=$(git --no-pager diff --name-only remotes/origin/$TARGET)
+}
+
+function iterate_files {
+  echo "Files found, iterating for *.spec files"
+  specs=0
+  IFS=' ' read -r -a $files <<< $files
+  for file in $files; do
+    echo "checking:" $file
+    if [[ $file =~ .*\.spec ]]; then
+      specs=$(($specs + 1))
+    fi
+  done
+  if [[ $specs = 0 ]]; then
+    echo "No spec files found."
+    result=1
+  else
+    echo "Spec files found"
+    result=0
+  fi
 }
 
 cd $GITHUB_WORKSPACE/
@@ -14,10 +33,6 @@ echo "TARGET:" $TARGET
 echo "Fetching..."
 git fetch --all --prune-tags
 echo "...done"
-echo "vvvvvvvvvvvvvvvv"
-git --no-pager diff --name-only remotes/origin/$TARGET
-echo "^^^^^^^^^^^^^^^^"
-echo 1
 
 get_files
 
@@ -26,22 +41,7 @@ then
   echo "Nothing Found";
   exit $?
 else
-  echo "Files found, iterating for *.spec files"
-  specs=0
-  IFS=' ' read -r -a $files <<< $files
-  for file in $files; do
-    echo "checking:" $file
-    if [[ $file =~ .*\.spec ]]; then
-      specs=$(($specs + 1))
-    fi
-  done
-  if [[ $specs = 0 ]]; then
-    echo "No spec files found."
-    echo "------------- Script Ending ----------------------"
-    exit 1
-  else
-    echo "Spec files found"
-    echo "------------- Script Ending ----------------------"
-    exit 0
-  fi
+  iterate_files
 fi
+echo "------------- Script Ending ----------------------"
+exit $result
