5f45443ac56439073a437db488edca7f558f5d9d
The-Nusbaum <38957691+The-Nusbaum@users.noreply.github.com>
2021-01-13 13:57:32 -0500

    forgot the meat and potatoes

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function get_files {
+  files=$(git --no-pager diff --name-only FETCH_HEAD)
+}
+
+cd $GITHUB_WORKSPACE/
+
+echo "------------- Script Starting ----------------------"
+
+git fetch --prune-tags
+
+get_files
+
+if [ -z "$files" ];
+then
+  echo "Nothing Found";
+  exit $?
+else
+  echo "Files found, iterating for *.spec files"
+  specs=0
+  export IFS=" "
+  for file in $files; do
+    if [[ $file =~ .*\.spec ]]; then
+      specs=$(($specs + 1))
+    fi
+  done
+  if [[ $specs = 0 ]]; then
+    echo "No spec files found."
+    echo "------------- Script Ending ----------------------"
+    exit 1
+  else
+    echo "Spec files found"
+    echo "------------- Script Ending ----------------------"
+    exit 0
+  fi
+fi
